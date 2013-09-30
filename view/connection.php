0b334e9f0d16679c32e80b9d9a0177105ae969db
AdvancedCreation <AdvancedCreation@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-09-30 12:23:09 +0000

    Mise à jour
    git-svn-id: https://plugins.svn.wordpress.org/shipworks-e-commerce-bridge/trunk@780276 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- view/connection.php
+++
@@ -1,4 +0,0 @@
-<?php
-header('Content-Type: text/xml');
-
-include_once('control/controlConnection.php');
--- view/control/controlSetUp.php
+++
@@ -1,7 +0,0 @@
-<?php
-// On affiche la page tutoriel sans accès à la base de donnée
-
-include_once(PLUGIN_PATH_SHIPWORKSWORDPRESS.'view/setUp.php');
-
-
-?>
